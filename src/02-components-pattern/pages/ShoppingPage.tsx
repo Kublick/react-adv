c7bebd76a9f1eaf9d623e7b7501c645b678850cf
Max Blancarte <blancarte@gmail.com>
2022-01-14 19:32:04 -0800

    Compound Component

---
+++ src/02-components-pattern/pages/ShoppingPage.tsx
@@ -0,0 +1,34 @@
+import {
+	ProductButtons,
+	ProductCard,
+	ProductImage,
+	ProductTitle,
+} from '../componens';
+
+const product = {
+	id: '1',
+	title: 'Coffee Mug',
+	img: './coffee-mug.png',
+};
+
+export const ShoppingPage = () => {
+	return (
+		<div>
+			<h1>Shopping Page</h1>
+			<hr />
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'row',
+					flexWrap: 'wrap',
+				}}
+			>
+				<ProductCard product={product}>
+					<ProductImage />
+					<ProductTitle title="" />
+					<ProductButtons />
+				</ProductCard>
+			</div>
+		</div>
+	);
+};
