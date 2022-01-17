0f38d667c9356399f072820d04fa9ae81013ebf0
Max Blancarte <blancarte@gmail.com>
2022-01-17 11:17:23 -0800

    control props alt

--- src/02-components-pattern/hooks/useShoppingCart.ts
+++ src/02-components-pattern/hooks/useShoppingCart.ts
@@ -17,30 +17,16 @@ export const useShoppingCart = () => {
 		product: Product;
 	}) => {
 		setShoppingCart((oldShoppingCart) => {
-			const productInCart: ProductInCart = oldShoppingCart[product.id] || {
-				...product,
-				count: 0,
-			};
-
-			if (Math.max(productInCart.count + count, 0)) {
-				productInCart.count += count;
-				return { ...oldShoppingCart, [product.id]: productInCart };
+			if (count === 0) {
+				const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+				return {
+					...rest,
+				};
 			}
-
-			//Borar producto
-			const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-			return { ...rest };
-
-			// 	if (count === 0) {
-			// 		const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-			// 		return {
-			// 			...rest,
-			// 		};
-			// 	}
-			// 	return {
-			// 		...oldShoppingCart,
-			// 		[product.id]: { ...product, count },
-			// 	};
+			return {
+				...oldShoppingCart,
+				[product.id]: { ...product, count },
+			};
 		});
 	};
 
